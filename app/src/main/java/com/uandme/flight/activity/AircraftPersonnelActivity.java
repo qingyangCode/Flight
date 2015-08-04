ed64a50eff759e337a36f40c2feec254524b8c3d
yangqing <yangqing@zepplabs.com>
2015-08-04 21:56:01 +0800

    同步代码

--- app/src/main/java/com/uandme/flight/activity/AircraftPersonnelActivity.java
+++ app/src/main/java/com/uandme/flight/activity/AircraftPersonnelActivity.java
@@ -2,11 +2,6 @@ package com.uandme.flight.activity;
 
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.util.Pair;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.CheckBox;
-import android.widget.TextView;
 import butterknife.InjectView;
 import com.uandme.autoloadlistview.LoadingFooter;
 import com.uandme.autoloadlistview.zlistview.ListViewAdapter;
